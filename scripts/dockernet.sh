586e7cb5085c1b8efb6d487c632dc9ad93cb8765
Daniel Dorofeev <dorofeev.d.spb@gmail.com>
2026-01-25 22:47:46 +0000

    modified:   scripts/dockernet.sh

--- scripts/dockernet.sh
+++ scripts/dockernet.sh
@@ -2,21 +2,31 @@
 
 
 
+
 # Проверка и создание macvlan интерфейса, если не существует
 if ! ip link show macvlan-CoreDNS &>/dev/null; then
   sudo ip link add macvlan-CoreDNS link eth0 type macvlan mode bridge
+  echo "Создан интерфейс macvlan-CoreDNS."
 else
   echo "Интерфейс macvlan-CoreDNS уже существует."
 fi
 
-# Проверка наличия IP-адреса на интерфейсе
-if ! ip addr show macvlan-CoreDNS | grep -q '172.20.100.254/24'; then
-  sudo ip addr add 172.20.100.254/24 dev macvlan-CoreDNS
+# Включение интерфейса, если он не up
+if ! ip link show macvlan-CoreDNS | grep -q 'state UP'; then
+  sudo ip link set macvlan-CoreDNS up
+  echo "Интерфейс macvlan-CoreDNS поднят."
 else
-  echo "IP 172.20.100.254/24 уже назначен на macvlan-CoreDNS."
+  echo "Интерфейс macvlan-CoreDNS уже поднят."
 fi
 
-sudo ip link set macvlan-CoreDNS up
+# Проверка наличия IP-адреса на интерфейсе (с любой маской)
+if ! ip addr show macvlan-CoreDNS | grep -q '172.20.100.254'; then
+  sudo ip addr add 172.20.100.254/24 dev macvlan-CoreDNS
+  echo "Назначен IP 172.20.100.254/24 на macvlan-CoreDNS."
+else
+  echo "IP 172.20.100.254 уже назначен на macvlan-CoreDNS."
+fi
+
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -P FORWARD ACCEPT
 
@@ -27,6 +37,7 @@ if ! sudo docker network ls --format '{{.Name}}' | grep -q '^macvlan-CoreDNS$'; 
     --gateway=172.20.100.254 \
     -o parent=eth0 \
     macvlan-CoreDNS
+  echo "Создана docker-сеть macvlan-CoreDNS."
 else
   echo "Docker-сеть macvlan-CoreDNS уже существует."
 fi
